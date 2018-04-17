f7f3e1b10a9ed0733afadc7239809433915efa57
Arun Gupta <arun.gupta@gmail.com>
2018-04-17 16:29:54 +0200

    howdy -> hello

--- src/main/java/org/aws/samples/compute/greeting/GreetingEndpoint.java
+++ src/main/java/org/aws/samples/compute/greeting/GreetingEndpoint.java
@@ -24,7 +24,7 @@ public class GreetingEndpoint {
         if (AWSXRay.getGlobalRecorder().getTraceEntity() != null)
             AWSXRay.getCurrentSegment().putAnnotation("parentId",
                     AWSXRay.getGlobalRecorder().getTraceEntity().getId());
-        String response = "Howdy";
+        String response = "Hello";
 
         return response;
     }
